a0c3acd976665d5e5e8c77b1ff7bd9a8d86f8659
VanillaPvPArchivist <hodaphnese@gmail.com>
2026-09-25 04:44:44 +0300

    final fixes

--- script.js
+++ script.js
@@ -53,7 +53,7 @@
       fun:   { name: "Mute (World of Roguecraft)", text: "", model: "models/rogue-fun.glb", lore: "The most influential vanilla PvP videos of all time, if you ever saw someone trying to flex on their enemies while naked, it's probably because of mute. (Released in reverse order, episode 3 was the first in the series)", video: "bqx1CFomKMI", animation: "Stand (ID 0 variation 0)" }
     },
     priest: {
-      skill: { name: "There are no good priests apparently", text: "", model: "models/priest-skill.glb", lore: "There are no good priests apparently", video: "X9737mnPejQ", animation: "Stand" },
+      skill: { name: "There are no good priests apparently", text: "", model: "models/priest-skill.glb", lore: "There are no good priests apparently", video: "IUQyClRWOFc", animation: "Stand" },
       fun:   { name: "Beckon", text: "", model: "models/priest-fun.glb", lore: "is the Hulksmash of holy priests, sit back, relax, and watch this man cast a 40 second holy fire to take someones head off", video: "x_EgBtUtWBM", animation: "Stand (ID 0 variation 0)" }
     },
     shaman: {
@@ -833,3 +833,8 @@ button.addEventListener('mouseleave', () => {
   // Hide the apple
   apple.style.display = 'none';
 });
+
+const homeBtn = document.getElementById('homeBtn');
+homeBtn.addEventListener('click', () => {
+  window.location.href = "index.html"
+  });
